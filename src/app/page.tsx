c1c981e691054ab6651848015329a798f2d78b39
zerotimeszero <you@example.com>
2024-03-15 16:37:47 +0100

    database set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
-import {appliances} from '@/lib/placeholder_data';
+import { sql } from "@vercel/postgres";
 
-export default function Home() {
+export default async function Home() {
+  const {rows} = await sql`SELECT * FROM Appliances`;
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Главная!</h1>
       <section>
         <h2 className="text-red-500">Оборудование</h2>
-        {JSON.stringify(appliances, null, 4)}
+        <div>
+          {rows.map((row: any) => (
+          <div className='flex gap-20' key={row.id}>
+            <div>{row.id}</div>
+            <div>{row.type}</div>
+            <div>{row.price}</div>
+          </div>
+      ))}
+    </div>
       </section>
     </main>
   );
